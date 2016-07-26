0ed94990066b6d605fcb67d7956687ec86db5076
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2016-07-26 00:12:39 -0700

    Checkpoints

--- notebooks/make_gatoperro.py
+++ notebooks/make_gatoperro.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+import os
 import numpy as np
 from scipy.misc import imsave, fromimage
 from PIL import Image, ImageOps
 from tqdm import tqdm, trange
 
-BASE = '/data/gatos'
+if os.path.exists('/data/gatos'):
+    BASE = '/data/gatos'
+else:
+    BASE = '../data/gatos'
+
 WIDTH = HEIGHT = 224
 def load_and_crop_image(filename, target_size):
     return ImageOps.fit(Image.open(filename), target_size)
@@ -38,32 +43,40 @@ all_feats = np.c_[np.ones(len(imagenes)), cat_probs, brightness]
 np.save('all_feats.npy', all_feats)
 np.save('train_indices.npy', first_set_of_images)
 
-feats = []
-true = []
-for i, idx in enumerate(first_set_of_images):
-    imsave('img_{:02}.jpg'.format(i), imagenes[idx])
-    # labels[idx] - .5 + np.random.standard_normal()*.1,
-    feats.append([1, cat_probs[idx], brightness[idx]])
-    true.append(labels[idx])
-true = np.array(true)
-feats = np.array(feats)
+def save_imgs(indices, fmt, filename):
+    feats = []
+    true = []
+    for i, idx in enumerate(indices):
+        imsave(fmt.format(i), imagenes[idx])
+        # labels[idx] - .5 + np.random.standard_normal()*.1,
+        feats.append([1, cat_probs[idx], brightness[idx]])
+        true.append(labels[idx])
+    true = np.array(true)
+    feats = np.array(feats)
 
-with open('page.html','w') as f:
-    print('''<style>
-        div { page-break-after: always; }
-        table { border-collapse: collapse; margin: 5px; }
-        td { border: 1px solid black; padding: 5px; }
-    </style>''', file=f)
-    for i, feat in enumerate(feats):
-        print('<div>', file=f)
-        print('<h1>{}</h1>'.format(i+1), file=f)
-        print('<img src="img_{:02}.jpg">'.format(i), file=f)
-        print(
-            '<table><tr>',
-            ''.join('<td>{:.02f}</td>'.format(f) for f in feat),
-            '</tr><tr>',
-            ''.join(['<td>&nbsp;</td>'] * len(feat)),
-            '</tr><tr>',
-            ''.join(['<td>&nbsp;</td>'] * len(feat)),
-            '</tr></table>', file=f)
-        print('</div>', file=f)
+    with open(filename,'w') as f:
+        print('''<style>
+            div { page-break-after: always; }
+            table { border-collapse: collapse; margin: 5px; }
+            td { border: 1px solid black; padding: 5px; }
+        </style>''', file=f)
+        for i, feat in enumerate(feats):
+            print('<div>', file=f)
+            print('<h1>{}</h1>'.format(i+1), file=f)
+            print('<img src="{}">'.format(fmt.format(i)), file=f)
+            print(
+                '<table><tr>',
+                ''.join('<td>{:.02f}</td>'.format(f) for f in feat),
+                '</tr><tr>',
+                ''.join(['<td>&nbsp;</td>'] * len(feat)),
+                '</tr><tr>',
+                ''.join(['<td>&nbsp;</td>'] * len(feat)),
+                '</tr></table>', file=f)
+            print('</div>', file=f)
+
+save_imgs(first_set_of_images, 'img_{:02}.jpg', 'page.html')
+
+unused = np.ones(len(imagenes), dtype=bool)
+unused[first_set_of_images] = 0
+second_set = np.random.choice(np.flatnonzero(unused), 20, replace=False)
+save_imgs(second_set, 'img2_{:02}.jpg', 'page2.html')
